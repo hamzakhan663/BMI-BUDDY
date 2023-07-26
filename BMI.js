a35d6c6d31ef16e53b0fc731a8a06a1002e88a68
hamzakhan663 <sulaimonhamza233@gmail.com>
2023-07-26 16:53:00 +0100

    Added Changes

--- BMI.js
+++ BMI.js
@@ -1,23 +1,47 @@
-let sleep = ms => {  
-    return new Promise(resolve => setTimeout(resolve, ms));  
-    };  
-      
-//
-sleep(2000).then(() => {
-console.log("Welcome to the BMI Buddy❤️‍🩹.")
-});
 const readline = require("readline");
 
-const rl = readline.createInterface({
+// Function to introduce a delay using a Promise
+const sleep = ms => {
+  return new Promise(resolve => setTimeout(resolve, ms));
+};
+
+// Main function to gather user input
+const main = async () => {
+  console.log("Welcome to the BMI Buddy❤️‍🩹.");
+
+  // Introduce a delay before asking for the user's name
+  await sleep(2000);
+
+  const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
-});
+  });
 
-sleep(2000).then(() => {
-    rl.question("Please what is your name?\n",function(name){
+  rl.question("Please what is your name?\n", function(name) {
+    console.log(`Hello ${name}, We are pleased to have you here.`);
+
+    // Introduce a delay before asking for weight
+    sleep(2000).then(() => {
+      rl.question("Kindly enter your weight in kilograms: ", function(weight) {
+        // Introduce a delay before asking for height
         sleep(2000).then(() => {
-        console.log("Hello " + name + ", We are pleased to have you here.");
+          rl.question("Kindly enter your height in meters: ", function(height) {
+            sleep(2000).then(() => {
+            console.log(`Your weight is ${weight}kg and your height is ${height}m.`);
+            
+            sleep(2000).then(() => {
+            console.log("Kindly await your BMI Result.");
+            
+            // Close the readline interface when done
+            rl.close();
+          });
+        });
         });
     });
-    
-});
+      });
+    });
+  });
+};
+
+// Run the BMI calculator
+main();
